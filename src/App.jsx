c6db988f532b2df378c9d58bdce6233ddc0e798d
Oshitha <osithadil55@gmail.com>
2025-04-23 15:23:29 +0530

    init ✏

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/header'
+import ProductCard from './components/productCard'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <ProductCard name="Apple Laptop" description="MACBook Air" price= "1000/=" picture="https://picsum.photos/id/2/200/300" />
+      <ProductCard name="Gaming Laptop" description="MSI laptop" price= "1500/=" picture="https://picsum.photos/id/3/200/300" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import UserData from "./userData";
+
+export default function Header() {
+    return (
+        <div className="bg-red-500">
+            <h1 className="text-[30px]">Crystal Beauty Clear</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quasi.</p>
+            <UserData />
+        </div>
+    )
+}
