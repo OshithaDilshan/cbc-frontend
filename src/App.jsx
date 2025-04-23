d8d35b8d8e5b1b2905d89d3cd8100798770f54af
Oshitha <osithadil55@gmail.com>
2025-04-23 15:32:33 +0530

    change some codes 👍

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
       <Header />
       <ProductCard name="Apple Laptop" description="MACBook Air" price= "1000/=" picture="https://picsum.photos/id/2/200/300" />
       <ProductCard name="Gaming Laptop" description="MSI laptop" price= "1500/=" picture="https://picsum.photos/id/3/200/300" />
+      <ProductCard name="work Laptop" description="MSI laptop" price= "1500/=" picture="https://picsum.photos/id/4/200/300" />
+
     </>
   )
 }
