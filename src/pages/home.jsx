187bcb5f930f9303e80e66c64d87cd7523dfce56
OshithaDilshan <osithadil55@gmail.com>
2025-05-28 22:38:19 +0530

    Day 16 & 17 ✏

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
 import ProductPage from "./client/productPage";
 import ProductOverviewPage from "./client/productOverview";
+import CartPage from "./client/cart";
+import CheckoutPage from "./client/checkOut";
 
 export default function HomePage() {
     return (
@@ -13,6 +15,8 @@ export default function HomePage() {
                     <Route path="/products" element={<ProductPage />} />
                     <Route path="/about" element={<h1>About</h1>} />
                     <Route path="/contact" element={<h1>Contact</h1>} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
                     <Route path="/overview/:id" element={<ProductOverviewPage />} />
                     <Route path="/*" element={<h1>404 Not Found</h1>} />
                 </Routes>
