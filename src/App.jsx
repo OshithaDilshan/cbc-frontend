f0815324becbe0e65fdff9ca07e20a4ae234412e
OshithaDilshan <osithadil55@gmail.com>
2025-07-28 10:48:47 +0530

    Day 20 ✏

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,29 @@ import RegisterPage from './pages/register'
 import LoginPage from './pages/login'
 import AdminPage from './pages/adminPage'
 import TestPage from './pages/testPage'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Toaster } from 'react-hot-toast'
+import ForgetPasswordPage from './pages/forgetPassword'
 
 function App() {
+  const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
   return (
-    <BrowserRouter>
-      <div>
-        <Toaster position='top-right' />
-        <Routes path="/*">
-          <Route path='/login' element={<LoginPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
-          <Route path='/testing' element={<TestPage />} />
-          <Route path="/admin/*" element={<AdminPage />} />
-          <Route path='/*' element={<HomePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <BrowserRouter>
+        <div>
+          <Toaster position='top-right' />
+          <Routes path="/*">
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/forget' element={<ForgetPasswordPage />} />
+            <Route path="/signup" element={<RegisterPage />} />
+            <Route path='/testing' element={<TestPage />} />
+            <Route path="/admin/*" element={<AdminPage />} />
+            <Route path='/*' element={<HomePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   )
 }
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,8 @@
+import { useGoogleLogin } from "@react-oauth/google"
 import axios from "axios"
 import { useState } from "react"
 import toast from "react-hot-toast"
+import { GrGoogle } from "react-icons/gr"
 import { useNavigate } from "react-router-dom"
 
 export default function LoginPage() {
@@ -9,12 +11,33 @@ export default function LoginPage() {
     const [password, setPassword] = useState("")
     const navigate = useNavigate()
 
+    const googleLogin = useGoogleLogin({
+        onSuccess: (response) => {
+            const accessToken = response.access_token
+            axios.post(import.meta.env.VITE_BACKEND_URL + "/api/users/login/google", {
+                accessToken: accessToken
+            }).then((response) => {
+                toast.success("Login Successful")
+                const token = response.data.token
+                localStorage.setItem("token", token)
+                if (response.data.role === "admin") {
+                    navigate("/admin/")
+                }
+                else {
+                    navigate("/")
+                }
+            })
+        }
+    })
+
+
     async function handleLogin() {
         try {
             const response = await axios.post(import.meta.env.VITE_BACKEND_URL + "/api/users/login", {
                 email: email,
                 password: password
             })
+            //alert("Login Successful")
             toast.success("Login Successful")
             console.log(response.data)
             localStorage.setItem("token", response.data.token)
@@ -27,8 +50,12 @@ export default function LoginPage() {
 
 
         } catch (e) {
+            //alert(e.response.data.message)
             toast.error(e.response.data.message)
         }
+
+
+
 
     }
 
@@ -49,10 +76,8 @@ export default function LoginPage() {
                         }
 
                         value={email}
-                        placeholder="Email"
 
-
-                        className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] my-[20px] px-4" />
+                        className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] my-[20px]" />
                     <input
                         onChange={
                             (e) => {
@@ -60,11 +85,12 @@ export default function LoginPage() {
                             }
                         }
                         value={password}
-                        placeholder="Password"
-
-                        type="password" className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] mb-[20px] px-4" />
+                        type="password" className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] mb-[20px]" />
                     <button onClick={handleLogin} className="w-[300px] cursor-pointer h-[50px] bg-[#c3efe9] rounded-[20px] my-[20px] text-[20px] font-bold text-white">Login</button>
-
+                    <button onClick={googleLogin} className="w-[300px] cursor-pointer h-[50px] flex justify-center items-center bg-[#c3efe9] rounded-[20px] my-[20px] text-[20px] font-bold text-white" >
+                        <GrGoogle className="text-xl text-gray-600 cursor-pointer hover:text-gray-800" />
+                        <span className="text-gray-600 text-xl font-semibold">Login with Google</span>
+                    </button>
                 </div>
 
             </div>
